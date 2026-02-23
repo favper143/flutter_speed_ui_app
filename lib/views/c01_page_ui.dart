bfb69504a96e4af591f0889531e70a88e1b708b0
favper143 <s6619410060@sau.ac.th>
2026-02-23 17:52:32 +0700

    commit 05 : ทำหน้า C01 เสร็จแล้ว แต่ยังไม่รู้ว่าต้องลิ้งยังไง

--- lib/views/c01_page_ui.dart
+++ lib/views/c01_page_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class C01PageUi extends StatefulWidget {
   const C01PageUi({super.key});
@@ -10,6 +11,74 @@ class C01PageUi extends StatefulWidget {
 class _C01PageUiState extends State<C01PageUi> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          // เลเยอร์ที่ 1: รูปภาพพื้นหลัง
+          Image.asset(
+            'assets/images/img8.png',
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          // เลเยอร์ที่ 2: เนื้อหา
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/img10.png',
+                  width: 95,
+                  height: 95,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'HOPE FOR',
+                  style: GoogleFonts.poppins(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'HUMANITY',
+                  style: GoogleFonts.poppins(
+                    height: 0.7,
+                    fontSize: 32,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Welcome to',
+                  style: GoogleFonts.poppins(
+                    fontSize: 30,
+                    color: Color(0xFF005014),
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  'hope for humanity',
+                  style: GoogleFonts.poppins(
+                    fontSize: 30,
+                    color: Color(0xFF005014),
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: 50,),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
